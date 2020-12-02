73105dd38f67a1a575c2e1351378e4090867bb05
liuwangb <liuwangb@dcits.com>
2020-12-02 22:47:40 +0800

    feat:两个字符串的最长公共子串

---
+++ 字符串/002.两个字符串的最长公共子串.py
@@ -0,0 +1,15 @@
+# 动态规划法
+def getMaxSubStr(s1, s2):
+    m = [[0 for i in range(len(s2) + 1)] for j in range(len(s1) + 1)]
+    maxL, count = 0, 0
+    for i in range(len(s1)):
+        for j in range(len(s2)):
+            if s1[i] == s2[j]:
+                m[i + 1][j + 1] = m[i][j] + 1
+                if m[i + 1][j + 1] > maxL:
+                    maxL = m[i + 1][j + 1]
+                    count = i + 1
+    return s1[count - maxL:count], maxL
+
+
+print(getMaxSubStr('abcdfg', 'abdfg'))
